6d37e6ee67d51f13c5f08f354255344e329eef44
Abdul Sohail <abdulsohail22246@gmail.com>
2021-07-30 18:34:26 +0530

    created basic structure of website

--- src/Home.js
+++ src/Home.js
@@ -10,28 +10,30 @@ function HomePage() {
 					<div className="row">
 						<div className="col-12 mx-auto">
 							{/* Text Area */}
-							<div className="col-md-6 sm-12 textarea">
-								<div className="main-text">
-									Don't worry. <br /> We are here for <br /> every solution.
+							<div class="row">
+								<div className="col-md-6 sm-12 order-sm-0 order-lg-0 textarea">
+									<div className="main-text">
+										Don't worry. <br /> We are here for <br /> every solution.
+									</div>
+									<br />
+									<div className="sub-text">
+										Tyger! Tyger! burning bright In the forests of the night,
+										What immortal hand or eye Could frame thy fearful symmetry?
+									</div>
+									<div className="button_space">
+										<button className="btn button_field col-sm-6 col-md-4 col-lg-3">
+											Get Started
+										</button>
+										<button className="btn second_button_field col-sm-6 col-md-4 col-lg-3">
+											Watch Video
+										</button>
+									</div>
 								</div>
-								<br />
-								<div className="sub-text">
-									Tyger! Tyger! burning bright In the forests of the night, What
-									immortal hand or eye Could frame thy fearful symmetry?
-								</div>
-								<div className="button_space">
-									<button className="btn button_field col-sm-6 col-md-4 col-lg-3">
-										Get Started
-									</button>
-									<button className="btn second_button_field col-sm-6 col-md-4 col-lg-3">
-										Watch Video
-									</button>
-								</div>
-							</div>
 
-							{/* Image Area */}
-							<div className="col-md-6 sm-12">
-								<img src="{happy}" className="img-fluid" alt="" />
+								{/* Image Area */}
+								<div className="col-md-6 sm-12 order-sm-1 order-lg-1">
+									<img src={happy} className="img-fluid" alt="" />
+								</div>
 							</div>
 						</div>
 					</div>
